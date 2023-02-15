7d2df7fe304dd4223c250e9f4d780da957d17efb
donnie <donnie4w@gmail.com>
2023-02-15 12:40:03 +0800

    v1

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # wfs-pyclient
-wfs的thrift python3实现访问接口
+**WFS的THRIFT PYTHON3实现访问接口**
+
+
+------------
+上传文件
+
+    获取wfs客户端实例：
+	wfs = WfsClient("http://127.0.0.1:3434/thrift")
+	
+    bs=getFileBytes("1.jpg")
+	#上传 文件bytes, 文件名
+    wfs.PostFile(bs,"22.jpg","") 
+	#相当于：curl -F "file=@1.jpg" "http://127.0.0.1:3434/u/22.jpg"
+	#1.jpg 是本地文件，22.jpg是上传到服务自定义的文件名，
+	#也可以：
+	wfs.PostFile(bs,"fff/ggg/1.jpg","") 
+	#访问则为：http://127.0.0.1:3434/r/fff/ggg/1.jpg
+	
+拉取 文件
+
+    f = wfs.GetFile("22.jpg")
+	#相当于：http://127.0.0.1:3434/r/22.jpg
+	 f = wfs.GetFile("fff/ggg/22.jpg")
+	#相当于：http://127.0.0.1:3434/r/fff/ggg/22.jpg
+    print(len(f.fileBody))
+	#保存文件到本地
+    saveFileByBytes(f.fileBody,"22_1.jpg")
+
+删除文件
+
+    wfs.DelFile("22.jpg")
+
---
+++ wfsclient.py
@@ -0,0 +1,46 @@
+from thrift.transport.THttpClient import THttpClient
+from thrift.protocol import TCompactProtocol
+import IWfs as IW
+
+
+class WfsClient:
+    def __init__(self,url) -> None:
+        self.serverUrl=url
+
+    def PostFile(self,bs,name,fileType):
+        transport = THttpClient(self.serverUrl)
+        protocol = TCompactProtocol.TCompactProtocol(transport)       
+        client = IW.Client(protocol)
+        transport.open() 
+        wf = IW.WfsFile()
+        wf.fileBody,wf.fileType,wf.name = bs,fileType,name
+        client.wfsPost(wf)
+       
+    def GetFile(self,name) :
+        transport = THttpClient(self.serverUrl)
+        protocol = TCompactProtocol.TCompactProtocol(transport)       
+        client = IW.Client(protocol)
+        transport.open() 
+        return client.wfsRead(name)
+
+    def DelFile(self,name):
+        transport = THttpClient(self.serverUrl)
+        protocol = TCompactProtocol.TCompactProtocol(transport)       
+        client = IW.Client(protocol)
+        transport.open() 
+        client.wfsDel(name)        
+
+def getFileBytes(filename):
+    return open(filename, "rb").read()
+def saveFileByBytes(bs,filename):
+    open(filename, "wb").write(bs)
+
+if __name__ == "__main__":
+    wfs = WfsClient("http://127.0.0.1:3434/thrift")
+    bs= getFileBytes("1.jpg")
+    wfs.PostFile(bs,"22","")
+    f = wfs.GetFile("22")
+    print(len(f.fileBody))
+    saveFileByBytes(f.fileBody,"22_1.jpg")
+    # wfs.DelFile("22")
+
